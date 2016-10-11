545f157129cb525306a3590e996670adf127d764
MTibby92 <MTibby92@gmail.com>
2016-10-11 10:13:53 -0500

    passed word from game.js to main.js

--- game.js
+++ game.js
@@ -1,5 +1,7 @@
 // selects a random word for the game
 var randomWord = require('random-word')
 
-var word = randomWord()
-console.log('The random word generated on line 5, file game.js is:', word)
+// var word = randomWord()
+// console.log('The random word generated on line 5, file game.js is:', word)
+
+module.exports = randomWord()
--- main.js
+++ main.js
@@ -1,1 +1,4 @@
 // logic for the game, where the game is called from
+var randomWord = require('./game.js')
+console.log('Console Log from main.js:', randomWord)
+console.log(typeof randomWord)
