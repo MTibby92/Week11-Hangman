826d2ecb60e05444661db8d06197f60ccfe15920
MTibby92 <MTibby92@gmail.com>
2016-10-11 10:40:34 -0500

    got word methods working in main.js

--- main.js
+++ main.js
@@ -2,3 +2,10 @@
 var randomWord = require('./game.js')
 console.log('Console Log from main.js:', randomWord)
 console.log(typeof randomWord)
+
+var Word = require('./word.js')
+var currentWord = new Word(randomWord)
+console.log(currentWord.letterArray)
+console.log(currentWord.charInWord())
+console.log(currentWord.letterArray)
+console.log(currentWord.checkWon())
--- word.js
+++ word.js
@@ -1,1 +1,19 @@
 // contains all of the methods which will check the letters guessed versus the random word selected
+module.exports = function Word(random) {
+	this.guess = process.argv[2]
+	this.letterArray = random.split('')
+	this.charInWord = function() {
+		for (var i in this.letterArray) {
+			if (this.letterArray[i] == this.guess) {
+				var removed = this.letterArray.splice(i, 1)
+			}
+		}
+	}
+	this.checkWon = function() {
+		if (this.letterArray.length == 0) {
+			return true
+		} else {
+			return false
+		}
+	}
+}
