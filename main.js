100941cf08c849353bad0d4dcdd23b480ba714e8
MTibby92 <MTibby92@gmail.com>
2016-10-11 11:06:27 -0500

    working on using the inquirer module, right now it's constantly calling instead of stopping after 15

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 // logic for the game, where the game is called from
+var inquirer = require('inquirer')
 var randomWord = require('./game.js')
 console.log('Console Log from main.js:', randomWord)
 console.log(typeof randomWord)
@@ -9,3 +10,28 @@ console.log(currentWord.letterArray)
 console.log(currentWord.charInWord())
 console.log(currentWord.letterArray)
 console.log(currentWord.checkWon())
+
+var counter = 15
+
+function runGame() {
+	while (counter > 0) {
+		console.log(currentWord.letterArray)
+		inquirer.prompt([
+		{
+			type: 'input',
+			name: 'guess',
+			message: 'Please enter your guess, you have ' + counter + ' tries remaining'
+		}
+			])
+		.then(function(answer) {
+			currentWord.charInWord()
+			console.log('Win?', currentWord.checkWon())
+			counter -= 1
+			if (counter > 0) {
+				runGame()
+			}
+		})
+	}
+}
+
+runGame()
